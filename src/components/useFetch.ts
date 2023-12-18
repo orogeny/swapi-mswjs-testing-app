6270b7c38b4f715256fc2c45207187029f418035
Paul <paulwharin@gmail.com>
2023-12-18 15:44:46 +0000

    refactor: mset loading flag to false in the finally block

--- src/components/useFetch.ts
+++ src/components/useFetch.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-
-const SWAPI_API_BASE = "https://swapi.dev/api" as const;
+import { SWAPI_BASE_URL } from "../environment_variables";
 
 type State<T> =
   | { loading: false; loaded: false; data: null }
@@ -8,7 +7,7 @@ type State<T> =
   | { loading: false; loaded: true; data: T };
 
 function useFetch<T>(url: string) {
-  const [data, setData] = useState<T | undefined>(undefined);
+  const [data, setData] = useState<T | null>(null);
   const [loading, setLoading] = useState(false);
   const [loaded, setLoaded] = useState(false);
 
@@ -19,9 +18,9 @@ function useFetch<T>(url: string) {
       try {
         setLoading(true);
         setLoaded(false);
-        setData(undefined);
+        setData(null);
 
-        const response = await fetch(`${SWAPI_API_BASE}${url}`, {
+        const response = await fetch(`${SWAPI_BASE_URL}${url}`, {
           headers: {
             "Content-Type": "application/json",
           },
@@ -30,17 +29,20 @@ function useFetch<T>(url: string) {
 
         const result = (await response.json()) as T;
 
-        console.log("got data: ", result);
-
         setData(result);
-        setLoading(false);
         setLoaded(true);
       } catch (error) {
         if (error instanceof DOMException) {
           console.log("Fetch was aborted");
         } else {
-          console.log("fetch error: ", error);
+          if (error instanceof Error) {
+            console.log(error.message);
+          } else {
+            console.log("fetch error: ", error);
+          }
         }
+      } finally {
+        setLoading(false);
       }
     }
 
@@ -49,7 +51,7 @@ function useFetch<T>(url: string) {
     return () => controller.abort();
   }, [url]);
 
-  return { loading, loaded, data: data ?? null } as State<T>;
+  return { loading, loaded, data } as State<T>;
 }
 
 export { useFetch };
