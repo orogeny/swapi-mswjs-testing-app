748dc381814f6a7f5581b7ee3ec72a24b5780c65
Paul <paulwharin@gmail.com>
2023-12-19 09:39:09 +0000

    feat: configure msw to be used in browser for development

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+console.log(`main.tsx::mode: ${import.meta.env.MODE}`);
+
+async function enableMocking() {
+  if (import.meta.env.MODE === "development") {
+    const { worker } = await import("./mock_api/browser.ts");
+
+    return worker.start();
+  }
+}
+
+enableMocking().then(() => {
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
